4b9653cebc9f7613c73da3f3d0d726199d225aa8
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-04-04 10:08:59 -0400

    created a generatemessage module and tested with mocha

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -14,26 +15,13 @@ app.use('/', express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
-    //socket.emit from: Admin, text: Welcome to the nodechatter app
-    socket.emit('userConnect', {
-        from: 'Admin',
-        text: 'Welcome to the nodechatter app',
-        createdAt: new Date().getTime()
-    });
-    //socket.broadcast.emit from: Admin, text: New User Joined
-    socket.broadcast.emit('userConnect', {
-        from: 'Admin',
-        text: 'New User Joined',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Node Chatter App'));
+    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,

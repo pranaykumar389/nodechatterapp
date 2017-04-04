55df6f7ea936a03d0e0eecf3d9792a3e57fa89f6
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-04-04 15:19:05 -0400

    created input field for messages and show incoming and outgoing messages in browser

--- client/js/index.js
+++ client/js/index.js
@@ -11,6 +11,21 @@ socket.on('userConnect', function (userConnect) {
     console.log('New User', userConnect);
 });
 
-socket.on('newMessage', function (newMessage) {
-    console.log('New Message', newMessage);
+socket.on('newMessage', function (message) {
+    console.log('New Message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });

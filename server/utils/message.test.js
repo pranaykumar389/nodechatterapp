50bac6b95aee067a50684dd7e7b66e1d24eafdda
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-04-04 16:26:54 -0400

    created geolocation suport via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 
@@ -13,4 +13,17 @@ describe('generateMessage', () => {
         expect(message).toInclude({from, text});
     });
 
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'John';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
 });

d6c134b5718347336098cbaf430dbba5f1d7f94c
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-04-05 00:23:47 -0400

    added style sheet and enable and disable functionality to send-location button

--- client/js/index.js
+++ client/js/index.js
@@ -7,12 +7,7 @@ socket.on('disconnect', function () {
     console.log('Disconnected from Server');
 });
 
-socket.on('userConnect', function (userConnect) {
-    console.log('New User', userConnect);
-});
-
 socket.on('newMessage', function (message) {
-    console.log('New Message', message);
     var li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
 
@@ -32,11 +27,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -46,12 +43,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 
